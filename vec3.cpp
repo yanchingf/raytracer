9903f6ae3ea8b99cb90baaa2feadc5aca801b293
yanchingf <s53668537@gmail.com>
2026-07-04 00:38:26 -0500

    initial

---
+++ vec3.cpp
@@ -0,0 +1,73 @@
+
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <cmath>
+
+struct vec3 {
+
+    double v[3];
+
+    vec3() : v{0, 0, 0} {}
+    vec3(double a, double b, double c) : v{a, b, c} {}
+
+    vec3 operator-(){
+        return vec3(-v[0], -v[1], -v[2]);
+    }
+
+    vec3& operator+=(const vec3& v2){
+        v[0] += v2.v[0];
+        v[1] += v2.v[1];
+        v[2] += v2.v[2];
+        return *this;
+    }
+
+    vec3& operator-=(const vec3& v2){
+        v[0] -= v2.v[0];
+        v[1] -= v2.v[1];
+        v[2] -= v2.v[2];
+        return *this;
+    }
+
+    vec3& operator*=(double c){
+        v[0] *= c;
+        v[1] *= c;
+        v[2] *= c;
+        return *this;
+    }
+
+    vec3& operator/=(double c){
+        v[0] /= c;
+        v[1] /= c;
+        v[2] /= c;
+        return *this;
+    }
+
+    double length(){
+        return sqrt(pow(v[0], 2) + pow(v[1], 2) + pow(v[2], 2));
+    }
+
+    vec3 normalized(const vec3& v){
+        double l = v.length()
+        return v
+    }
+
+    double r() const {return v[0]; }
+    double g() const {return v[1]; }
+    double b() const {return v[2]; }
+
+    double x() const {return v[0]; }
+    double y() const {return v[1]; }
+    double z() const {return v[2]; }
+
+
+
+    
+
+
+
+
+    
+};
+
+#endif
